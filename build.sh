4bf9173135379341fb1ef32ef1d933eee73e7b3e
mertyn <mweber28@hs-mittweida.de>
2024-04-08 18:00:24 +0200

    add preset selection

--- build.sh
+++ build.sh
@@ -1,22 +1,40 @@
 #!/usr/bin/env bash
 
+preset="$1"
+version_suffix="$2"
+
 project="cyborg-cat-carnage"
 version="1.1"
-version_suffix="rc1"
 rev=$(git rev-parse --short HEAD)
-platform="web"
+
+declare -A file_names=(
+	["windows"]="$project.exe"
+	["linux"]="$project.x86_64"
+	["web"]="index.html"
+)
+
+
+if [[ -z $preset ]]; then
+	echo "error: no preset given, exiting."
+	exit
+fi
+
+if [[ -z ${file_names[$preset]} ]]; then
+	echo "error: preset $preset not recognized, exiting."
+	exit
+fi
 
 
 echo "building $project version $version$version_suffix at revision $rev"
-echo "building platform $platform"
+echo "building preset $platform"
 
-name="$project-v$version$version_suffix-$rev-$platform"
+name="$project-v$version$version_suffix-$rev-$preset"
 path="build/$name"
 
 mkdir -p "$path"
 
 echo "building project..."
-godot4 --path ./src --headless --export-release "Web" "../$path/index.html"
+godot4 --path ./src --headless --export-release "$preset" "../$path/${file_names[$preset]}"
 echo
 
 echo "creating build archive..."
